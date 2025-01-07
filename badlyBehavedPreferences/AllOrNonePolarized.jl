d2ac3b250455162d1a7a961a6336b7bebab40a52
Jonathan R. Warden <john.warden@gmail.com>
2025-01-07 19:38:48 +0100

    Remove badlyBehavedMechanisms and badlyBehavedPreferences

--- badlyBehavedPreferences/AllOrNonePolarized.jl
+++
@@ -1,6 +0,0 @@
-# Can't just use a step function cause optimizers assume these are curves.
-return make_preference_profile([
-    x -> x[1] >= .99 ? 100*x[1] : .01*x[1],
-    x -> x[2] >= .99 ? 100*x[2] : .02*x[2],
-], 2)
-
